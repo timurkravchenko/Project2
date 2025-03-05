ac473e91bcdf58b51ddfaa6ef75595c608135c4b
timurkravchenko <34355@iitu.edu.kz>
2025-03-05 10:39:25 +0500

    small changes

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,12 +4,14 @@ import Header from '../header'
 import RandomPlanet from '../random-planet'
 import ItemList from '../item-list'
 import PersonDetails from '../person-details'
-
+import SwapiService from "../../services/swapi-service";
 import './app.css';
 import ErrorIndicator from "../error-indicator";
 import PeoplePage from "../people-page";
 
 export default class App extends Component {
+
+    swapiService = new SwapiService()
 
             state = {
                 selectedPerson: null,
@@ -30,6 +32,27 @@ export default class App extends Component {
             <RandomPlanet/>
 
             <PeoplePage />
+            <div className="row mb2">
+                <div className="col-md-6">
+                    <ItemList
+                        onItemSelected={this.onPersonSelected}
+                        getData={ this.swapiService.getAllPlanets }/>
+                </div>
+                <div className="col-md-6">
+                    <PersonDetails personId={this.state.selectedPerson} />
+                </div>
+            </div>
+
+            <div className="row mb2">
+                <div className="col-md-6">
+                    <ItemList
+                        onItemSelected={this.onPersonSelected}
+                        getData={ this.swapiService.getAllStarships }/>
+                </div>
+                <div className="col-md-6">
+                    <PersonDetails personId={this.state.selectedPerson} />
+                </div>
+            </div>
         </div>
     )
     }
